8659c1c2c224b12e10956f463e6143cbd04a7e10
JordyDanielCh <danielchicaiza2303@gmail.com>
2024-07-13 22:12:42 -0500

    Delete PrincipalModel.php

--- PrincipalModel.php
+++
@@ -1,5 +0,0 @@
-//obtener producto a partir de la lista de deseo
-public function getListaDeseo($id_producto) {
-    $sql = "SELECT * FROM productos WHERE id = $id_producto";
-    return $this->select($sql);
-}
